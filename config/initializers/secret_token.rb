8644901b7ed5a521d6f993db6ef82417645b6945
Maxzy <daxiongyun@creditease.cn>
2013-06-18 04:28:45 +0800

    CPU个人会员V3.0产品设计

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CPUV30::Application.config.secret_token = '410abeffe42abacc09626a020be84063e945d96ec5ef856a3b17ae806ad277cc2c6c5d9402dd94931afc14d7f4cf69496147f73b41872bbe648b0defa04350e1'
